729535a404247041576990608f6b3aaf28b2fbe4
akshatjainaccolite <59680388+akshatjainaccolite@users.noreply.github.com>
2020-01-29 12:33:10 +0530

    Add files via upload

---
+++ 21-01-2020 FN SOAP/SoapAssignment/src/main/java/com/spring/accolite/SoapAssignment/SoapAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.spring.accolite.SoapAssignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoapAssignmentApplication.class, args);
+	}
+
+}
